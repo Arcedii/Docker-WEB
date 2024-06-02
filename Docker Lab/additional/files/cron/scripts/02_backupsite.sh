bfa2e257465111b71caaa21fcce67a94f8e4bf0d
Arcedii <91275440+Arcedii@users.noreply.github.com>
2024-06-02 20:21:46 +0300

    delete

--- Docker Lab/additional/files/cron/scripts/02_backupsite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "[backup] create site backup" \
-    > /proc/1/fd/1 \
-    2> /proc/1/fd/2
-tar czfv /var/backups/site/www_$(date +\%Y\%m\%d).tar.gz /var/www/html
-echo "[backup] site backup done" \
-    >/proc/1/fd/1 \
-    2> /proc/1/fd/2
--- Docker Lab/additional/files/cron/scripts/environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-env >> /etc/environment
-
-# execute CMD
-echo "Start cron" >/proc/1/fd/1 2>/proc/1/fd/2
-echo "$@"
-exec "$@"
